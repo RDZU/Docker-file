47d431e86183bcec251306aa3dcbd1d489082876
Randolph David (razamora) <davidzamorau@gmail.com>
2023-12-25 20:20:14 +0100

    templates

---
+++ server_apache_php/html/index.php
@@ -0,0 +1,34 @@
+<?php
+echo "text";
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <title>Plantilla Rápida HTML</title>
+</head>
+<body>
+  <header>
+    <h1>Título de la página</h1>
+    <nav>
+      <a href="#">Enlace 1</a>
+      <a href="#">Enlace 2</a>
+      <a href="#">Enlace 3</a>
+    </nav>
+  </header>
+  <main>
+    <section>
+      <h2>Título de la sección</h2>
+      <p>Contenido de la sección</p>
+    </section>
+    <section>
+      <h2>Título de la sección</h2>
+      <p>Contenido de la sección</p>
+    </section>
+  </main>
+  <footer>
+    <p>Copyright © 2023</p>
+  </footer>
+</body>
+</html>
